b09718f96a6bc68efc22cdd0f7992f9cd6071d5a
5bondarenko <bondarenko16.12.2000@mail.ru>
2021-02-10 18:45:21 +0300

    Optimized code

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,47 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[27]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-def my_plotter(ax, data1, data2, param_dict):
-    out = ax.plot(data1, data2, **param_dict)
-    return out
 
-#input points of triangle
-a = np.array([1, 1]) 
-b = np.array([1, 4])
-c = np.array([4, 4])
-
-#getting figure
-figure = np.array([a, b, c])
-
-#converting figure into axis
-x = np.array([figure[0][0], figure[1][0], figure[2][0], figure[0][0]])
-y = np.array([figure[0][1], figure[1][1], figure[2][1], figure[0][1]])
-
-#plotting
-fig, ax = plt.subplots()
-my_plotter(ax, x, y, {'marker' : 'o'})
-
-#transformation matrix
-T = np.array([[1, 0], [0, 1]])
-T = T * np.array([-1, -1])
-
-#transforming
-figure = figure @ T
-
-#converting figure into axis
-x = np.array([figure[0][0], figure[1][0], figure[2][0], figure[0][0]])
-y = np.array([figure[0][1], figure[1][1], figure[2][1], figure[0][1]])
-
-my_plotter(ax, x, y, {'marker' : 'o'})
+def plot_contour(points, params=None):
+    if params is None:
+        params = {}
+    plt.plot(np.append(points[:, 0], [points[0, 0]]),
+             np.append(points[:, 1], [points[0, 1]]),
+             **params)
 
 
+def main():
+    # input points
+    points = np.array([[float(elem) for elem in input(f'Введите точку {i}: ').split(',')]
+                       for i in range(1, int(input('Введите количество точек: ')) + 1)])
+
+    # plotting
+    plt.figure(1)
+    plot_contour(points)
+
+    # transformation matrix
+    T = np.array([[1, 0], [0, 1]])
+    T = T * np.array([-1, -1])
+    points = np.dot(points, T)
+
+    # plotting again
+    plot_contour(points)
+    plt.title("Отображение фигуры")
+    plt.show()
 
 
-
-
+if __name__ == "__main__":
+    main()
